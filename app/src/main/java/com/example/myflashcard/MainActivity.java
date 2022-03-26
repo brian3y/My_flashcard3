1ffe446f2aa9b524638b32d007a3450a36ffd933
brian <brianstimeisnow@gmail.com>
2022-03-25 18:41:05 -0700

    Lab 2

--- app/src/main/java/com/example/myflashcard/MainActivity.java
+++ app/src/main/java/com/example/myflashcard/MainActivity.java
@@ -1,19 +1,23 @@
 package com.example.myflashcard;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView flashcardQuestion;
+    TextView flashcardAnswer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView flashcardQuestion = findViewById(R.id.textView4);
-        TextView flashcardAnswer = findViewById(R.id.textView5);
+         flashcardQuestion = findViewById(R.id.textView4);
+         flashcardAnswer = findViewById(R.id.textView5);
         flashcardAnswer.setVisibility(View.INVISIBLE);
         flashcardQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,5 +26,30 @@ public class MainActivity extends AppCompatActivity {
                 flashcardAnswer.setVisibility(View.VISIBLE);
             }
         });
+        ImageView addQuestionImageView = findViewById(R.id.flashcard_add_button);
+
+        addQuestionImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                //startActivity(intent);
+                startActivityForResult(intent,100);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==100){
+            if(data!=null){
+                String question=data.getExtras().getString("Question_key");
+                String answer=data.getExtras().getString("Answer_key");
+                flashcardQuestion.setText(question);
+                flashcardAnswer.setText(answer);
+            }
+        }
     }
 }
